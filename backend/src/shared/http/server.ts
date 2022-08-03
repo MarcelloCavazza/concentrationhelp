001d2294d84d44194520588f830b582187852b9e
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-08-02 23:53:16 -0300

    create basic route

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log("oi");
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+const serverPort = process.env.SERVER_PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log(`Server started on http://localhost:3000`);
+});
