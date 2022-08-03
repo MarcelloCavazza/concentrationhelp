f956d5ac0d65463a1b2a5770f9647a30e66f647c
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-08-03 00:13:50 -0300

    create global error treatment

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,13 +1,28 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
-const serverPort = process.env.SERVER_PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, reponse: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return reponse.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return reponse.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3000, () => {
   console.log(`Server started on http://localhost:3000`);
